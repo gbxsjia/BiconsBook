e2af019d7f480d083b9970c002952755b912b7a6
Huacong Tang <40313381+TINKPA@users.noreply.github.com>
2021-02-07 02:06:14 -0800

    j

--- Bicon's Book/Assets/Scripts/Equipments/EM_Item.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Equipment/Item")]
-public class EM_Item : Equipment
-{
-    public int ConsumeTimes;
-    public Sprite[] ItemSprite = new Sprite[12];
-}
