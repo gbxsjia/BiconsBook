8658e1ff69bb749ead3cf5d8a8331c7f7cae9ea7
BeixiJia <240793032@qq.com>
2021-02-08 13:56:41 +0800

    清库

--- Bicon's Book/Assets/Scripts/Equipments/EM_Armor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Equipment/Armor")]
-public class EM_Armor : Equipment
-{
-    
-}
--- Bicon's Book/Assets/Scripts/Equipments/EM_Helmet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Equipment/Helmet")]
-public class EM_Helmet : Equipment
-{
-    public bool HideHair;
-}
